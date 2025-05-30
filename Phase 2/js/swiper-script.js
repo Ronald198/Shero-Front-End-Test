063cfef0413134b396e482324039b313f4a74ecf
Ronald198 <ronaldlamani@gmail.com>
2025-05-30 15:47:43 +0200

    Initial commit

---
+++ Phase 2/js/swiper-script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 3,
+  speed: 1400,
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 1,
+    depth: 250,
+    // modifier: 1,
+    slideShadows: false,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
